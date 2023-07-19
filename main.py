3a5ae5bbb51887961af56c3fb9669b488944e80e
KarolG <gitesiek@gmail.com>
2023-07-19 03:27:13 +0200

    Changed template, added ability to change res

--- main.py
+++ main.py
@@ -4,11 +4,54 @@ import numpy as np
 import random
 import time
 
-template_path = 'template.png'
-template_mori_path = 'template_mori.png'
+nobg_template = 'nobg_template.png'
+template = cv2.imread(nobg_template)
+threshold = 0.8
+
 key_to_reel_the_line = '4'
 key_to_cast_the_line = '2'
-threshold = 0.8
+
+base_res_x = 1280
+base_res_y = 800
+
+template_x = 135
+template_y = 42
+
+resolutions = [
+    [1024, 768],  # not working
+    [1280, 1024],  # not working
+    [1280, 800],  # base res
+    [1440, 900],  # working
+    [1024, 700],  # working but slow(?)
+    [1680, 1050],  # working
+]
+
+
+def choose_resolution():
+    print("Choose your game resolution:")
+    for idx, res in enumerate(resolutions):
+        print(idx, res)
+    user_res = input()
+
+    return resolutions[int(user_res)]
+
+
+def resize_template(user_resolution):
+    new_x_scale = user_resolution[0]/base_res_x
+    # new_x_scale = 0.95 # fix for res 0
+    new_y_scale = user_resolution[1]/base_res_y
+    print(user_resolution[0], user_resolution[1])
+    print(new_x_scale, new_y_scale)
+    new_x = template_x * new_x_scale
+    new_y = template_y * new_y_scale
+    new_dim = [int(new_x), int(new_y)]
+
+    resized_template = cv2.resize(template, new_dim)
+    print(new_dim)
+    return resized_template
+
+
+template = resize_template(choose_resolution())
 
 
 def is_animation_present(template, screenshot):
@@ -26,30 +69,19 @@ def is_animation_present(template, screenshot):
 
 
 counter = 0
-press2_x = 1222
-press2_y = 915
-
-press4_x = 1311
-press4_y = 915
-
-
+print("ENTER THE FISH")
 while True:
     screenshot = pyautogui.screenshot()
-
     screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
 
-    template = cv2.imread(template_mori_path)
     is_present, match_loc = is_animation_present(template, screenshot)
     if is_present:
         counter += 1
-        if counter >= 1:
-            delay = random.randint(50, 150) / 1000
-            # time.sleep(delay)
+        if counter >= 2:
             print("PULL OUT!")
             print(key_to_reel_the_line)
-            pyautogui.typewrite('4')
             counter = 0
             delay = random.randint(4000, 6500) / 1000
-            # time.sleep(delay)
+            time.sleep(delay)
             print("-------------")
-            pyautogui.moveTo(press4_x, press4_y)
+            print(key_to_cast_the_line)
